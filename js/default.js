e67c894ecd4992aa02b515d6e18c240b8fcdc8df
Saad Ansari <isaadansari@gmail.com>
2018-03-02 13:57:12 +0500

    a

--- js/default.js
+++ js/default.js
@@ -1,65 +1,63 @@
 (function (window, document, $, undefined) {
     var app = {
-            WINDOW_HEIGHT: $(window).height(),
-            WINDOW_WIDTH: $(window).width(),
-            isMobile: false,
-            isTouch: false,
-            isTablet: false,
-            resizeTimeoutID: null,
-            $body: $("body"),
-            isMouseDown: false,
-            slider: null,
-            $container: null,
-            gridSlider: [],
-            owl: null,
-            allLoaded: false,
-            pre_loading_api: "",
-            factory_scene_api: "",
-            products_scene_api: "",
-            people_scene_api: "",
-            sustainability_scene_api: "",
-            contact_scene_api: "",
-            owl_cc_grid_main: "",
-            previewMode: $("body").hasClass("preview"),
-            validMobileDevice: $(window).width() <= 1024,
-            detectDevice: function () {
-                (function (a) {
-                    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) {
-                        app.isMobile = true;
-                    }
-                })(navigator.userAgent || navigator.vendor || window.opera);
-                if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-                    app.isTouch = true;
-                    app.$body.addClass("touch");
-                } else {
-                    app.$body.addClass("no-touch");
+        WINDOW_HEIGHT: $(window).height(),
+        WINDOW_WIDTH: $(window).width(),
+        isMobile: false,
+        isTouch: false,
+        isTablet: false,
+        resizeTimeoutID: null,
+        $body: $("body"),
+        isMouseDown: false,
+        slider: null,
+        $container: null,
+        gridSlider: [],
+        owl: null,
+        allLoaded: false,
+        pre_loading_api: "",
+        factory_scene_api: "",
+        products_scene_api: "",
+        people_scene_api: "",
+        sustainability_scene_api: "",
+        contact_scene_api: "",
+        owl_cc_grid_main: "",
+        previewMode: $("body").hasClass("preview"),
+        validMobileDevice: $(window).width() <= 1024,
+        detectDevice: function () {
+            (function (a) {
+                if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) {
+                    app.isMobile = true;
                 }
-                app.isTablet = (!app.isMobile && app.isTouch);
-            },
-            
-            _windowResize: function () {
-                app.WINDOW_HEIGHT = $(window).height();
-                app.WINDOW_WIDTH = $(window).width();
-            },
-
- 
-            initCountDown: function () {
-var nextYear = moment.tz("2015-01-01 00:00", "America/Sao_Paulo");
-
-$('#clock').countdown(nextYear.toDate(), function(event) {
-  $(this).html(event.strftime('%D days %H:%M:%S'));
-});                
-            },
-            init: function () {
-				app.detectDevice();
-                app.initCountDown();
+            })(navigator.userAgent || navigator.vendor || window.opera);
+            if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+                app.isTouch = true;
+                app.$body.addClass("touch");
+            } else {
+                app.$body.addClass("no-touch");
             }
+            app.isTablet = (!app.isMobile && app.isTouch);
+        },
+
+        _windowResize: function () {
+            app.WINDOW_HEIGHT = $(window).height();
+            app.WINDOW_WIDTH = $(window).width();
+        },
+
+
+        initCountDown: function () {
+            var nextYear = moment.tz("2018-03-02 20:00", "America/Sao_Paulo");
+
+            $('#clock').countdown(nextYear.toDate(), function (event) {
+                $(this).html(event.strftime('%D days %H:%M:%S'));
+            });
+        },
+        init: function () {
+            app.detectDevice();
+            app.initCountDown();
         }
-    ;
+    };
     window.app = app;
 })
 (window, document, jQuery);
 $(document).ready(function () {
-	app.init();   
+    app.init();
 });
-
